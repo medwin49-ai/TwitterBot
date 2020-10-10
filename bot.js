9c43c0e74d3448f059746b773590093a80ef8d5c
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2020-10-09 22:51:35 -0700

    added the npm package

---
+++ bot.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+var Twit = require('twit');
+
+var t = new Twit({
+    consumer_key:         '...'
+  , consumer_secret:      '...'
+  , access_token:         '...'
+  , access_token_secret:  '...'
+})

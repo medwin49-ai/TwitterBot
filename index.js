d544044e0ba954020df2be9eecf61993e4286d9b
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2020-10-11 00:53:25 -0700

    Images now can be posted

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var Twit = require('twit')
-
+var Twit = require('twit');
+var fs= require('fs');
+var exec =require('child_process').exec;
 var T = new Twit({
   consumer_key:         'om3V4t27xPDXuO82Z8yD9N94H',
   consumer_secret:      'a1WWqJ0BLW3u02K4h1fnHsF47gfIUhZLbTkjfxmZMyP7hpoNn0',
@@ -7,23 +8,42 @@ var T = new Twit({
   access_token_secret:  'LkZrlsrsXnsnOFO8u9GLsWQU9ZJIYq3ZfoGopI9E6R7RZ',
 })
 
-var tweet = { 
-    status: 'hello world!'
+processing();
+
+  function processing(){
+      var filename = 'Krabs/Mr.Krabs_.png';
+      var params ={
+          encoding: 'base64'
+      }
+      var b64content = fs.readFileSync(filename, params);
+      T.post('media/upload', { media_data: b64content }, uploaded);
+
+      function uploaded(err , data, response){
+        var id = data.media_id_string ;
+        var tweet = {
+            status:' Test',
+            media_ids: [id]
+        }
+        T.post('statuses/update', tweet, tweeted);
+    }
+
+        function tweeted (err, data, response) {
+
+            if ( err){
+                console.log(data);
+                console.log('Something went wrongs.');
+            }
+            else{
+                console.log('Ahoy there Sailor!');
+            }
+    }
 }
+  
 
 
 
-T.post('statuses/update', tweet, tweered );
+// first we must post the media to Twitter
+ 
 
-
-function tweered (err, data, response) {
-    console.log(data)
-    if ( err){
-        console.log(data);
-        console.log('Something went wrongs.');
-    }
-    else{
-        console.log('Ahoy there Sailor!');
-    }
-
-  }
+    // now we can assign alt text to the media, for use by screen readers and
+  // other text-based presentations and interpreter

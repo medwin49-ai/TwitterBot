bdd193f7e14c26e76d012524b3df16c76fa96c31
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2020-10-10 23:09:48 -0700

    add the keys

--- bot.js
+++ bot.js
@@ -1,9 +1,24 @@
-console.log('Hello World');
-var Twit = require('twit');
 
-var t = new Twit({
-    consumer_key:         '...'
-  , consumer_secret:      '...'
-  , access_token:         '...'
-  , access_token_secret:  '...'
+var Twit = require('twit');
+var T = new Twit({
+    consumer_key:         'hdo3H2E0gV3GbzX7SMnDkJ58L'
+  , consumer_secret:      'Agl8vstdZM8fh4FAqnytYwZKLqxUB7HPjJwrzrYM9FUIMtZtoP'
+  , access_token:         '1315161035075018753-julJqgGQv239e1VTJOp8mLJvJ237P4'
+  , access_token_secret:  'rQxmGEuIzgkdiNJPJk0ezyfOdtiBZDdaS4qUx076IQa6U'
 })
+
+
+var tweet = {
+  status: 'hello world!'
+}
+
+T.post('statuses/update', tweet  , tweeted);
+
+function tweeted (err, data, response) {
+  if(err){
+    console.log("Something went wrong");
+  }
+  else{
+    console.log("Ahoy!");
+  }
+}

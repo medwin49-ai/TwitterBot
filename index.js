b2f60881f0ae9eac5e87960ea15065c2c5f24e2c
Edwin Mendoza <MENDOZA_EDWIN@student.smc.edu>
2020-10-17 21:50:21 -0700

    clean up

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 var Twit = require('twit');
+var configs = require('./configs.js');
 var fs= require('fs');
-var exec =require('child_process').exec;
-var T = new Twit({
-  consumer_key:         'om3V4t27xPDXuO82Z8yD9N94H',
-  consumer_secret:      'a1WWqJ0BLW3u02K4h1fnHsF47gfIUhZLbTkjfxmZMyP7hpoNn0',
-  access_token:         '1315161035075018753-zg3kQcJTWHidoxPmSTt9nIB2L3rLXY',
-  access_token_secret:  'LkZrlsrsXnsnOFO8u9GLsWQU9ZJIYq3ZfoGopI9E6R7RZ',
-})
 
-processing();
 
-  function processing(){
-      var filename = 'Krabs/Mr.Krabs_.png';
-      var params ={
-          encoding: 'base64'
+
+var T = new Twit(configs)
+
+
+
+Processing();
+function Processing(){
+    var filename = 'Krabs/VidoeofKrabs.mp4'
+    var params ={
+        encoding: 'base64'
+    }
+  
+    var b64content = fs.readFileSync(filename, params);
+    var filePath = 'Krabs/VidoeofKrabs.mp4'
+   
+    
+    T.post('media/upload', { media_data: b64content }, uploaded);
+
+    function uploaded(err , data, response){
+      var id = data.media_id_string ;
+     
+      var tweet = {
+          status:'comon',
+          media_ids: [id]
       }
-      var b64content = fs.readFileSync(filename, params);
-      T.post('media/upload', { media_data: b64content }, uploaded);
 
-      function uploaded(err , data, response){
-        var id = data.media_id_string ;
-        var tweet = {
-            status:' Test',
-            media_ids: [id]
-        }
-        T.post('statuses/update', tweet, tweeted);
-    }
+      T.postMediaChunked({ file_path: filePath }, function(err,data,response){})+
+      T.post('statuses/update', tweet, tweeted);
+  }
 
-        function tweeted (err, data, response) {
+      function tweeted (err, data, response) {
 
-            if ( err){
-                console.log(data);
-                console.log('Something went wrongs.');
-            }
-            else{
-                console.log('Ahoy there Sailor!');
-            }
-    }
+          if ( err){
+              console.log(data);
+              console.log('Something went wrongs.');
+          }
+          else{
+              console.log('Ahoy there Sailor!');
+          }
+  }
 }
   
 
